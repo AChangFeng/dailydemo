9eb73fed43a2d2bcd480c7584bc7c45600c06e78
ChangFeng <1048243380@qq.com>
2018-11-23 08:26:56 +0800

    add：remove useless plugin and dependency;add bubble sort and insert sort;

--- src/main/java/wind/datastruct/SortDemo.java
+++ src/main/java/wind/datastruct/SortDemo.java
@@ -19,7 +19,7 @@ public class SortDemo {
         });
         //insertionSort(array);
         //shellSort(array);
-        bubbleSort(array);
+        bubbleSortV0(array);
         printArray(array);
     }
 
@@ -63,7 +63,7 @@ public class SortDemo {
      *
      * @param array
      */
-    public static void insertionSort(int[] array) {
+    public static void insertSortV0(int[] array) {
         // 索引0上的本来就是有序的 不需排序
         for (int i = 1; i < array.length; i++) {
             int j;
@@ -79,12 +79,41 @@ public class SortDemo {
     }
 
     /**
+     * 插入排序
+     *
+     * @param a
+     */
+    private static void insertSortV1(int[] a) {
+        if (a.length <= 1) {
+            return;
+        }
+        // 索引0上的本来就是有序的 不需排序
+        for (int i = 1; i < a.length; i++) {
+            // 将下标为i的元素插入到左边的已有序部分中
+            int value = a[i];
+            // 已有序部分的最后一个下标为i-1
+            int j = i - 1;
+            // 在已有序部分中找到要插入的位置 同时将大于需插入元素右移1位
+            for (; j >= 0; j--) {
+                if (a[j] > value) {
+                    a[j + 1] = a[j];
+                } else {
+                    break;
+                }
+            }
+            // 插入 该元素
+            a[j + 1] = value;
+        }
+    }
+
+    /**
      * 冒泡排序
      *
      * @param array
      */
-    public static void bubbleSort(int[] array) {
+    public static void bubbleSortV1(int[] array) {
         for (int i = 0; i < array.length; i++) {
+            // 记录一次冒泡是否进行了元素操作 如果没有操作 说明已经排序完毕
             boolean flag = false;
             // 一次冒泡的最大次数为：当前需排序元素-1，因为每次冒泡比较2个元素
             // 当前需排序元素就是j-i
@@ -98,6 +127,26 @@ public class SortDemo {
             }
             if (!flag) {
                 break;
+            }
+        }
+    }
+
+    /**
+     * 冒泡排序
+     *
+     * @param a
+     */
+    private static void bubbleSortV0(int[] a) {
+        if (a.length <= 1) {
+            return;
+        }
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a.length - i - 1; j++) {
+                if (a[j] > a[j + 1]) {
+                    int tmp = a[j + 1];
+                    a[j + 1] = a[j];
+                    a[j] = tmp;
+                }
             }
         }
     }
