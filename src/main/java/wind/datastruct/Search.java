1e7f6ea76ff8db854983aee032a96763114296cb
ChangFeng <18702601420@163.com>
2018-12-10 14:43:06 +0800

    add:binary search variants

--- src/main/java/wind/datastruct/Search.java
+++ src/main/java/wind/datastruct/Search.java
@@ -31,4 +31,104 @@ public class Search {
         return -1;
     }
 
+    /**
+     * 查找第一个等于val的元素
+     *
+     * @param a
+     * @param val
+     * @return
+     */
+    private static int binarySearchFirst(int[] a, int val) {
+        int low = 0, high = a.length - 1;
+        while (low <= high) {
+            int mid = low + ((high - low) >> 2);
+            if (a[mid] < val) {
+                low = mid + 1;
+            } else if (a[mid] > val) {
+                high = mid - 1;
+            } else {
+                if (mid == 0 || a[mid - 1] != val) {
+                    return mid;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 查找最后一个等于val的元素
+     *
+     * @param a
+     * @param val
+     * @return
+     */
+    private static int binarySearchLast(int[] a, int val) {
+        int low = 0, high = a.length - 1;
+        while (low <= high) {
+            int mid = low + ((high - low) >> 2);
+            if (a[mid] < val) {
+                low = mid + 1;
+            } else if (a[mid] > val) {
+                high = mid - 1;
+            } else {
+                if (mid == a.length - 1 || a[mid + 1] != val) {
+                    return mid;
+                } else {
+                    low = mid + 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 查找第一个大于等于val的元素
+     *
+     * @param a
+     * @param val
+     * @return
+     */
+    private static int binarySearchFirstGranterThanOrEqual(int[] a, int val) {
+        int low = 0, high = a.length - 1;
+        while (low <= high) {
+            int mid = low + ((high - low) >> 2);
+            if (a[mid] < val) {
+                low = mid + 1;
+            } else {
+                if (mid == 0 || a[mid - 1] < val) {
+                    return mid;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 查找最后一个小于等于val的元素
+     *
+     * @param a
+     * @param val
+     * @return
+     */
+    private static int binarySearchLastLessThanOrEqual(int[] a, int val) {
+        int low = 0, high = a.length - 1;
+        while (low <= high) {
+            int mid = low + ((high - low) >> 2);
+            if (a[mid] <= val) {
+                if (mid == a.length || a[mid + 1] > val) {
+                    return mid;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+
 }
